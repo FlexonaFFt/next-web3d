bff426695e1f9ce4a5f6be027893a54d8396e4ea
FlexonaFFt <flexonafft@bk.ru>
2024-07-24 21:12:56 +0300

    --load spline-home-model

---
+++ frontend/src/app/components/spline-home-model.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function SplineHome() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/YjTVRuo0Iia7A1zz/scene.splinecode" 
+      />
+    </main>
+  );
+}
