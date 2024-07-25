b881eb683c6bf83eff8dc36ecd994648fc3edb9c
FlexonaFFt <flexonafft@bk.ru>
2024-07-25 14:17:37 +0300

    --load test second block

---
+++ frontend/src/app/components/intro-block.js
@@ -0,0 +1,9 @@
+import '../styles/intro-block-styles.css';
+
+export default function IntroBlock() {
+    return (
+     <div className="block-settings">
+      <h1>Hello World</h1>
+     </div>
+  );
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,12 @@
 import SplineScene from './components/spline-home-model.js';
+import IntroBlock from './components/intro-block.js';
 import './App.styles.css';
 
 export default function App() {
   return (
    <main>
       <SplineScene />
+      <IntroBlock />
    </main> 
   );
 }
