fd8bc4d16afe7864e6b1e916b99cb9bda68027f8
FlexonaFFt <flexonafft@bk.ru>
2024-07-26 20:43:06 +0300

    --load bug fix relative

--- frontend/src/app/components/landing-part.js
+++ frontend/src/app/components/landing-part.js
@@ -6,24 +6,23 @@ export default function LandingPart() {
     return (
      <div className='content-container'>
       <div className='introduce'>
-        <div className='text-group'>
-          <p className='text-section'>An innovative solution for those who 
-            are looking for inspiration and want to share wisdom with othe
-            rs. Advanced artificial intelligence technologies to offer you
-             unique and original quotes on any topic.</p>
-          <div className='big-text-section introduce-text'>
-            <p className='intro-text-first'>We create</p>
-            <p className='intro-text-second'>quotes like</p>
-            <p className='intro-text-third'>a pro & more</p>
-            <p className='intro-text-quad'>efficient</p>
-          </div>
+        <div className='big-text-section introduce-text'>
+          <p className='intro-text-first'>We create</p>
+          <p className='intro-text-second'>quotes like</p>
+          <p className='intro-text-third'>a pro & more</p>
+          <p className='intro-text-quad'>efficient</p>
         </div>
-      </div>
+        <p className='text-section'>An innovative solution for those who 
+          are looking for inspiration and want to share wisdom with othe
+          rs. Advanced artificial intelligence technologies to offer you
+          unique and original quotes on any topic.</p>
 
-      <div className='post-introduce-image-block'>
+        <div className='post-introduce-image-block'>
         <Image src={ BookImage } alt='glasses-image' width={500} height={300} 
         className='custom-book-image'/>
+       </div>
       </div>
+
      </div>
   );
 }
