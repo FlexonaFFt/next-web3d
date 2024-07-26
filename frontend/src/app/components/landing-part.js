1a51ad296ca86f87e0b205a35939a0c8ecb81a5f
FlexonaFFt <flexonafft@bk.ru>
2024-07-26 12:52:28 +0300

    --load part of web work

--- frontend/src/app/components/landing-part.js
+++ frontend/src/app/components/landing-part.js
@@ -2,8 +2,21 @@ import '../styles/landing-styles.css';
 
 export default function LandingPart() {
     return (
-     <div className="block-settings">
-      <h1>Hello World</h1>
+     <div className='content-container'>
+      <div className='introduce'>
+        <div className='text-group'>
+          <p className='text-section'>An innovative solution for those who 
+            are looking for inspiration and want to share wisdom with othe
+            rs. Advanced artificial intelligence technologies to offer you
+             unique and original quotes on any topic.</p>
+          <div className='big-text-section introduce-text'>
+            <p className='intro-text-first'>We create</p>
+            <p className='intro-text-second'>quotes like</p>
+            <p className='intro-text-third'>a pro & more</p>
+            <p className='intro-text-quad'>efficient</p>
+          </div>
+        </div>
+      </div>
      </div>
   );
 }
